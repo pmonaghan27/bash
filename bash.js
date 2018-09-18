b296dc35778626378f713e655cac39d4c79b4764
jstoobz <stephensja7@gmail.com>
2018-09-18 13:09:25 -0400

    updates

--- bash.js
+++ bash.js
@@ -1,15 +1,20 @@
 const pwd = require('./pwd');
 const ls = require('./ls');
+const cat = require('./cat');
 
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', data => {
   const cmd = data.toString().trim();
 
+  const file = cmd.slice(4);
+
   if (cmd === 'pwd') {
     pwd();
   } else if (cmd === 'ls') {
     ls();
+  } else if (cmd.slice(0, 3) === 'cat') {
+    cat(file);
   } else {
     process.stdout.write('Invalid command: ' + cmd);
   }
